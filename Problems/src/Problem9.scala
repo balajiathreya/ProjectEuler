c1dcfe9fefc48ef6055ee72f7dc3d9c68c2af0ee
bathreya <bathreya@ebay.com>
2013-09-06 17:30:19 -0700

    problem 9 - initial commit

--- Problems/src/Problem9.scala
+++ Problems/src/Problem9.scala
@@ -1,3 +1,5 @@
+import math.BigDecimal.double2bigDecimal
+
 /**
  * author: balaji-xubuntu
  *
@@ -8,7 +10,19 @@
  * 32 + 42 = 9 + 16 = 25 = 52. There exists exactly one Pythagorean triplet for which a + b + c = 1000.
  * Find the product abc.
  *
+ * Formula for pythagorean triplet:
+ * a = 2n + 1, b = 2n(n + 1), c = 2n(n + 1) + 1
  */
+
 object Problem9 {
 
+	def main(args : Array[String]){
+		val squares = 0.to(1000).map(i => {
+			val sqrt = Math.sqrt(i)
+			if (sqrt)
+		})
+		for (i <- 0.to(1000); j <- (i+1).to(1000)){
+			if ()
+		}
+	}
 }
